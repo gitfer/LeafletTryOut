3eca2aeb231f344b0a547e7159d05d2adb38511e
Federico Rota <federico@codiceplastico.com>
2013-08-20 16:31:13 +0200

    Upper casing

--- controllers/MyController.js
+++ controllers/MyController.js
@@ -1,7 +1,7 @@
-angular.module('tryout', []).controller('MyController', ['$scope', '$http', function($scope, $http){
+angular.module('tryout', []).controller('MyController', ['$scope', '$http', '$filter', function($scope, $http, $filter){
 	$scope.comuneInserito = 'ZONE';
 	$scope.interroga = function(){
-		$http.get('/comuni?name='+$scope.comuneInserito, function(err){}).success(function(data){
+		$http.get('/comuni?name='+$filter('uppercase')($scope.comuneInserito), function(err){}).success(function(data){
 			console.log(data[0]);
 			L.geoJson(data[0]).addTo(map);
 		});
